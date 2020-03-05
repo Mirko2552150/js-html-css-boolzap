090fe25dfde90788e72b8510f9ae302f2554f4e6
Mirko <mirko.longo.1987@gmail.com>
2020-03-05 18:08:56 +0100

    Ricerca contatti OK

--- js/main.js
+++ js/main.js
@@ -7,8 +7,34 @@ $(".fa-microphone-alt").click(function(){ // al click su incona MIC?
     messaggio.find('.text-mess').text(nomeInput);
     messaggio.find('.ora-messaggio').text(time);
     $('.rig-main').append(messaggio);
+
+    setTimeout(function(){
+        var messaggioBoot = $('.templateAuto .message').clone();
+        messaggioBoot.find('.text-mess').text("Ciao! Ora non posso risponderti, ci sentiamo dopo");
+        messaggioBoot.find('.ora-messaggio').text(time);
+        $('.rig-main').append(messaggioBoot);
+    }, 3000);
+
 });
 
+
+$("#cerca-contatti").keyup(function(event){ //alla pressione accatra un evento
+    var carattereFiltro = $(this).val().toLowerCase(); // prendo il valore del selezionato
+    console.log(carattereFiltro); // vediamo quello che clikkiamo sulla console
+    $('.box-mess').each(function(){
+        var nome = $(this).find(".nomi-contatti").text().toLowerCase();
+        if(nome.includes(carattereFiltro)) {
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+    });
+});
+
+
+$(".icon-top-lef-rig .fa-search").click(function(){
+    $(".lef-up-not").slideToggle();
+});
 
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
